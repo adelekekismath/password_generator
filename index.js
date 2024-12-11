b8c9ef1d27122da513da4645f135ef310cdaa14d
adelekekismath9 <adelekekismath9@gmail.com>
2024-12-11 15:19:13 +0100

    Refactor password generation logic; improve UI elements and copy functionality

--- index.js
+++ index.js
@@ -1,61 +1,77 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g",
-    "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
+const numbers = "0123456789".split("");
+const symbols = "~`!@#$%^&*()_-+={[}]|:;<>,.?/".split("");
 
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const password1Elt = document.getElementById("password-1");
+const password2Elt = document.getElementById("password-2");
+const passwordLength = document.getElementById("password-length");
+const symbolsElt = document.getElementById("symbols");
+const numbersElt = document.getElementById("numbers");
+const copiedElt = document.getElementById("copied");
 
-const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+passwordLength.addEventListener("keydown", (event) => {
+    const allowedKeys = ["Backspace", "Tab", "ArrowLeft", "ArrowRight"];
+    if (!allowedKeys.includes(event.key)) {
+        event.preventDefault();
+    }
+});
 
-let password1Elt = document.getElementById("password-1");
-let password2Elt = document.getElementById("password-2");
-let passwordLenght = document.getElementById("password-lenght");
-let symbolsElt = document.getElementById("symbols");
-let numbersElt = document.getElementById("numbers");
+function generatePasswords() {
+    let length = parseInt(passwordLength.value) || 15;
+    if (symbolsElt.checked) length--;
+    if (numbersElt.checked) length--;
 
-function generatePasswords(){
-    let lenght = parseInt(passwordLenght.value);
- 
-    lenght = lenght ? lenght: 15;
-    lenght = symbolsElt.value ? lenght-1 : lenght;
-    lenght = numbersElt.value ? lenght-1 : lenght;
+    let firstPassword = generatePassword(length);
+    let secondPassword = generatePassword(length);
 
-    console.log(lenght);
-
-    let firstPassword = "";
-    let secondPassword = "";
-
-    for(let i=0; i < lenght; i++ ){
-        firstPassword += characters[getRandomIndex(characters.length)];
-        secondPassword += characters[getRandomIndex(characters.length)];
+    if (symbolsElt.checked) {
+        firstPassword += getRandomElement(symbols);
+        secondPassword += getRandomElement(symbols);
     }
 
-    if (symbolsElt.value) {
-        firstPassword += symbols[getRandomIndex(symbols.length)]
-        secondPassword += symbols[getRandomIndex(symbols.length)]
+    if (numbersElt.checked) {
+        firstPassword += getRandomElement(numbers);
+        secondPassword += getRandomElement(numbers);
     }
 
-    if (numbersElt.value) {
-        firstPassword += numbers[getRandomIndex(numbers.length)]
-        secondPassword += numbers[getRandomIndex(numbers.length)]
-    }
-
-    password1Elt.textContent = firstPassword;
-    password2Elt.textContent = secondPassword;
+    appendPassword(password1Elt, firstPassword, copyOnClipBoardFirstPassword);
+    appendPassword(password2Elt, secondPassword, copyOnClipBoardSecondPassword);
 }
 
-function getRandomIndex(length){
-    console.log(length);
-    return Math.floor( Math.random() * length)
+function generatePassword(length) {
+    return Array.from({ length }, () => getRandomElement(characters)).join("");
 }
 
-function copyOnClipBoard(password){
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function appendPassword(element, password, copyFunction) {
+    element.textContent = password;
+    const copyIcon = createCopyIcon();
+    copyIcon.addEventListener("click", copyFunction);
+    element.appendChild(copyIcon);
+}
+
+function createCopyIcon() {
+    const copyIcon = document.createElement("span");
+    copyIcon.classList.add("copy-icon");
+    copyIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="18" width="15.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#63E6BE" d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z"/></svg>`;
+    return copyIcon;
+}
+
+function copyOnClipBoard(password) {
     navigator.clipboard.writeText(password);
-    alert("Copied the text: " + password);
+    copiedElt.hidden = false;
+    setTimeout(() => {
+        copiedElt.hidden = true;
+    }, 3000);
 }
 
-function copyOnClipBoardFirstPassword(){
+function copyOnClipBoardFirstPassword() {
     copyOnClipBoard(password1Elt.textContent);
 }
 
-function copyOnClipBoardSecondPassword(){
+function copyOnClipBoardSecondPassword() {
     copyOnClipBoard(password2Elt.textContent);
 }
