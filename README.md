c3537cad91c10d700182e61dfdc641c88d686ebe
adelekekismath9 <adelekekismath9@gmail.com>
2025-03-12 11:41:03 +0100

    Enhance password generator features and improve UI layout

--- README.md
+++ README.md
@@ -12,5 +12,10 @@ This is a simple password generator that generates a password based on the user'
 ## Features
 - User can choose the length of the password
 - User can include special characters
+- User can include numbers
+- User can include uppercase letters
+- User can include lowercase letters
 - User can copy the password to the clipboard
+- User can see the password strength
+
 
--- index.js
+++ index.js
@@ -1,81 +1,75 @@
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
-const numbers = "0123456789".split("");
-const symbols = "~`!@#$%^&*()_-+={[}]|:;<>,.?/".split("");
+const passwordDisplay = document.getElementById("password-display");
+const lengthInput = document.getElementById("password-length");
+const lengthValue = document.getElementById("length-value");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolsCheckbox = document.getElementById("symbols");
+const generateBtn = document.getElementById("generate-btn");
+const strengthBar = document.getElementById("strength-bar");
+const strengthText = document.getElementById("strength-text");
 
-const password1Elt = document.getElementById("password-1");
-const password2Elt = document.getElementById("password-2");
-const passwordLength = document.getElementById("password-length");
-const symbolsElt = document.getElementById("symbols");
-const numbersElt = document.getElementById("numbers");
-const copiedElt = document.getElementById("copied");
-const generateButton = document.getElementById("generator-btn");
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "~`!@#$%^&*()_-+={[}]|:;<>,.?/";
 
-passwordLength.addEventListener("keydown", (event) => {
-    const allowedKeys = ["Backspace", "Tab", "ArrowLeft", "ArrowRight"];
-    if (!allowedKeys.includes(event.key)) {
-        event.preventDefault();
-    }
+lengthInput.addEventListener("input", () => {
+    lengthValue.textContent = lengthInput.value;
 });
 
-generateButton.addEventListener("click", generatePasswords);
+generateBtn.addEventListener("click", () => {
+    const length = parseInt(lengthInput.value);
+    const includeUppercase = uppercaseCheckbox.checked;
+    const includeLowercase = lowercaseCheckbox.checked;
+    const includeNumbers = numbersCheckbox.checked;
+    const includeSymbols = symbolsCheckbox.checked;
 
+    let characters = "";
+    if (includeUppercase) characters += uppercaseLetters;
+    if (includeLowercase) characters += lowercaseLetters;
+    if (includeNumbers) characters += numbers;
+    if (includeSymbols) characters += symbols;
 
-function generatePasswords() {
-    let length = parseInt(passwordLength.value) || 15;
-    if (symbolsElt.checked) length--;
-    if (numbersElt.checked) length--;
-
-    let firstPassword = generatePassword(length);
-    let secondPassword = generatePassword(length);
-
-    if (symbolsElt.checked) {
-        firstPassword += getRandomElement(symbols);
-        secondPassword += getRandomElement(symbols);
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
-    if (numbersElt.checked) {
-        firstPassword += getRandomElement(numbers);
-        secondPassword += getRandomElement(numbers);
-    }
+    passwordDisplay.textContent = password;
+    updateStrengthIndicator(password);
+});
 
-    appendPassword(password1Elt, firstPassword, copyOnClipBoardFirstPassword);
-    appendPassword(password2Elt, secondPassword, copyOnClipBoardSecondPassword);
+function updateStrengthIndicator(password) {
+    const strength = calculatePasswordStrength(password);
+    strengthBar.style.width = `${strength * 25}%`;
+    strengthText.textContent = ["WEAK", "MEDIUM", "STRONG", "VERY STRONG"][strength - 1];
 }
 
-function generatePassword(length) {
-    return Array.from({ length }, () => getRandomElement(characters)).join("");
+function calculatePasswordStrength(password) {
+    let strength = 0;
+    if (password.length >= 12) strength++;
+    if (/[A-Z]/.test(password)) strength++;
+    if (/[0-9]/.test(password)) strength++;
+    if (/[^A-Za-z0-9]/.test(password)) strength++;
+    return strength;
 }
 
-function getRandomElement(array) {
-    return array[Math.floor(Math.random() * array.length)];
+function copyPassword() {
+    const password = passwordDisplay.textContent;
+    if(!password) return;
+    navigator.clipboard.writeText(password).then(() => {
+        showCopiedMessage();
+    });
 }
 
-function appendPassword(element, password, copyFunction) {
-    element.textContent = password;
-    const copyIcon = createCopyIcon();
-    copyIcon.addEventListener("click", copyFunction);
-    element.appendChild(copyIcon);
-}
+function showCopiedMessage() {
+    const copiedMessage = document.createElement("div");
+    copiedMessage.id = "copied-message";
+    copiedMessage.textContent = "Copied!";
+    document.body.appendChild(copiedMessage);
 
-function createCopyIcon() {
-    const copyIcon = document.createElement("span");
-    copyIcon.classList.add("copy-icon");
-    copyIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="18" width="15.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#63E6BE" d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z"/></svg>`;
-    return copyIcon;
-}
-
-function copyOnClipBoard(password) {
-    navigator.clipboard.writeText(password);
-    copiedElt.hidden = false;
     setTimeout(() => {
-        copiedElt.hidden = true;
-    }, 3000);
-}
-
-function copyOnClipBoardFirstPassword() {
-    copyOnClipBoard(password1Elt.textContent);
-}
-
-function copyOnClipBoardSecondPassword() {
-    copyOnClipBoard(password2Elt.textContent);
+        copiedMessage.remove();
+    }, 2000);
 }
